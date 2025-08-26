4fc0fa44d9098b6b5bd2c4f50a2ebe64ead57ea9
Maedot Amha <maedotamha@gmail.com>
2025-08-26 14:49:07 +0300

    ui components and user food menu start

---
+++ dineq-frontend/src/app/user/restaurant/food-display/page.tsx
@@ -0,0 +1,12 @@
+import FoodHeader from '@/components/user/Header'
+import React from 'react'
+import from
+const FoodCard = () => {
+  return (
+    <div>FoodCard
+        <FoodHeader image= {}
+    </div>
+  )
+}
+
+export default FoodCard
