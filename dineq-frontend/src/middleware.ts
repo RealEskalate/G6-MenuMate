b8e676be92a50274280d57651d18d8e4449ca3a5
Maedot Amha <maedotamha@gmail.com>
2025-08-27 11:33:32 +0300

    authentication

---
+++ dineq-frontend/src/middleware.ts
@@ -0,0 +1,51 @@
+
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export default withAuth(
+  async function middleware(request: NextRequest) {
+    const token = request.nextauth.token;
+    const pathname = request.nextUrl.pathname;
+
+    console.log("Middleware triggered");
+    
+    if (!token) {
+      console.log(`Middleware: No token, redirecting to /signin from ${pathname}`);
+      return NextResponse.redirect(new URL('/signin', request.url));
+    }
+
+    const role = token?.role || 'applicant';
+
+    const roleRedirects: { [key: string]: string } = {
+      OWNER: '/restaurant',
+      CUSTOMER: '/user',
+    };
+
+    const redirectUrl = roleRedirects[role] || '/';
+
+    if (pathname.startsWith('/restaurant') && role !== 'OWNER') {
+      console.log(`Middleware: Unauthorized admin access by ${role}, redirecting to ${redirectUrl}`);
+      return NextResponse.redirect(new URL(redirectUrl, request.url));
+    }
+    if (pathname.startsWith('/user') && role !== 'CUSTOMER') {
+      console.log(`Middleware: Unauthorized manager access by ${role}, redirecting to ${redirectUrl}`);
+      return NextResponse.redirect(new URL(redirectUrl, request.url));
+    }
+    
+
+    return NextResponse.next();
+  },
+  {
+    pages: {
+      signIn: '/signin',
+    },
+  }
+);
+
+export const config = {
+  matcher: [
+    '/restaurant/:path*',
+    '/user/:path*',
+  ],
+};
