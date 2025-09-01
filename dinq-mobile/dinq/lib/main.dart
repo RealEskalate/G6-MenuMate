456b05af023aad0683b4c8040be3fb81e70a4f7c
Mitiku Melkamsew Seid <mitikumelkamsew@gmail.com>
2025-09-01 14:59:49 +0300

    feat(restaurant): removed the ui

--- dinq-mobile/dinq/lib/main.dart
+++ dinq-mobile/dinq/lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'core/injection.dart' as di;
-import 'core/routing/app_routing.dart';
 import 'core/temp/app_config.dart';
-import 'core/utils/theme.dart';
 import 'features/restaurant_management/presentation/bloc/restaurant_bloc.dart';
 
 void main() async {
@@ -25,20 +23,8 @@ class MyApp extends StatelessWidget {
           create: (context) => di.sl<RestaurantBloc>(),
         ),
       ],
-      child: MaterialApp(
+      child: const MaterialApp(
         title: 'DinQ',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: AppColors.primaryColor,
-          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
-          scaffoldBackgroundColor: Colors.grey[100],
-          appBarTheme: const AppBarTheme(
-            backgroundColor: AppColors.primaryColor,
-            foregroundColor: Colors.white,
-          ),
-        ),
-        initialRoute: '/explore',
-        onGenerateRoute: generateRoute,
       ),
     );
   }
