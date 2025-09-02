d60cce94fa65f37533c70b8c986121905e02b842
Maedot Amha <maedotamha@gmail.com>
2025-09-02 15:24:49 +0300

    change

--- dineq-frontend/src/app/page.tsx
+++ dineq-frontend/src/app/page.tsx
@@ -1,23 +1,15 @@
 import LandingPage from "./landing_page/page";
-// import Restaurants from "./user/restaurant-display/page";
-// import WhoAreYou from "@/components/common/WhoAreYou";
-// import EmailVerification from "@/components/common/EmailVerification";
-// import FoodCard from "./user/restaurant-display/food-display/page";
-// import SingleRestaurant from "./user/restaurant-display/[id]/page";
-// import NavBar from "@/components/common/NavBar";
+import ProfilePage from "./user/profile/page";
+
 
 
 
 export default function Home() {
   return (
     <div>
-      {/* <NavBar role={"user"}/> */}
       <LandingPage />
-      {/* <Restaurants/> */}
-      {/* <WhoAreYou/> */}
-      {/* <EmailVerification/> */}
-      {/* <FoodCard/> */}
-      {/* <SingleRestaurant/> */}
+      {/* <ProfilePage/> */}
+
     </div>
   );
 }
--- dineq-frontend/src/app/user/profile/page.tsx
+++ dineq-frontend/src/app/user/profile/page.tsx
@@ -91,9 +91,13 @@ const ProfilePage = () => {
       {/* Profile Card */}
       <div className="border rounded-lg p-6 shadow-sm space-y-6">
         <div className="flex flex-col items-center">
+          {session?.user?.image ? (
+
+          ):(
           <div className="w-24 h-24 rounded-full bg-gray-200 flex items-center justify-center text-gray-500">
             User
           </div>
+          )}
           <h2 className="mt-4 text-lg font-semibold">Abebe Kebede</h2>
           <p className="text-sm text-gray-500">Member since January 2024</p>
 
