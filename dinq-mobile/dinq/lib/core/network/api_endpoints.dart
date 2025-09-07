4f375096d74467920e73b9b703a61dec6428a141
kidus <kidusefrem2@gmail.com>
2025-09-07 12:50:38 +0300

    working qr scanner

--- dinq-mobile/dinq/lib/core/network/api_endpoints.dart
+++ dinq-mobile/dinq/lib/core/network/api_endpoints.dart
@@ -1,8 +1,10 @@
 // lib/core/network/api_endpoints.dart - TEMPORARY
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiEndpoints {
   // Temporary CORS proxy solution
-  static const String baseUrl = 'https://g6-menumate.onrender.com/api/v1';
-  
+  static  String baseUrl = dotenv.env['BASE_URL']!;
+
   // Or alternative proxies:
   // static const String baseUrl = 'https://api.allorigins.win/raw?url=https://g6-menumate.onrender.com/api/v1';
   // static const String baseUrl = 'https://cors-anywhere.herokuapp.com/https://g6-menumate.onrender.com/api/v1';
