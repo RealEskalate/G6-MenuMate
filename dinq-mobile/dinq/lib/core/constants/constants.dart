60dd69f4e28ea3018b930c9b5db48041b62a7681
kidus <kidusefrem2@gmail.com>
2025-09-08 10:19:20 +0300

    url edit

--- dinq-mobile/dinq/lib/core/constants/constants.dart
+++ dinq-mobile/dinq/lib/core/constants/constants.dart
@@ -1,5 +1,6 @@
 import '../network/api_endpoints.dart';
 
-const String baseUrl = ApiEndpoints.baseUrl;
+// Using a getter function instead of const to avoid constant evaluation error
+String get baseUrl => ApiEndpoints.baseUrl;
 const String accessToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uZG9lQGV4YW1wbGUuY29tIiwiZXhwIjoxNzU3MjU5MTY3LCJpc192ZXJpZmllZCI6ZmFsc2UsInJvbGUiOiJPV05FUiIsInN0YXR1cyI6IkFDVElWRSIsInN1YiI6IjY4Yjk1MDBjMWViZTUxNGI5OTQyNTIyNSJ9.KS4HUdaOnhuEYsO8wn2E36fLOG4RLKMaB_S0P_OR-jk';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uZG9lQGV4YW1wbGUuY29tIiwiZXhwIjoxNzU3MzI1MjA5LCJpc192ZXJpZmllZCI6ZmFsc2UsInJvbGUiOiJPV05FUiIsInN0YXR1cyI6IkFDVElWRSIsInN1YiI6IjY4Yjk1MDBjMWViZTUxNGI5OTQyNTIyNSJ9.F16oP278TQDIw9Ivn6pQWA0DCwjmSNjPAo527IVwhtg';
